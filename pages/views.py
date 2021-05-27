0d37116db135273540ae4bee66a3568d30ef389b
atlasfoo@lin <jf_mejiar547@live.com>
2021-05-27 16:03:28 -0600

    update and delete CBV

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from .models import Page
 
@@ -15,5 +15,18 @@ class PageDetailView(DetailView):
 class CreatePageView(CreateView):
     model = Page
     fields = ['title', 'content', 'order', ]
+    success_url = reverse_lazy('pages:pages')
 
+
+class UpdatePageView(UpdateView):
+    model = Page
+    fields = ['title', 'content', 'order', ]
+    template_name_suffix = "_update_form"
+
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+
+
+class DeletePageView(DeleteView):
+    model = Page
     success_url = reverse_lazy('pages:pages')
