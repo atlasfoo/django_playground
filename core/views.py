2c9f6f22602a81b756a8908c86c29a735ce33510
atlasfoo@lin <jf_mejiar547@live.com>
2021-05-26 23:09:26 -0600

    CBV rollback

--- core/views.py
+++ core/views.py
@@ -2,12 +2,9 @@ from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
 
-class HomePageView(TemplateView):
-    template_name = "core/home.html"
-
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'title': "Mi Super Web Playground"})
+def home(request):
+    return render(request, "core/home.html")
 
 
-class SamplePageView(TemplateView):
-    template_name = "core/sample.html"
+def sample(request):
+    return render(request, "core/sample.html")
