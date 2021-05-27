e491f3eeba8765116d0ca30b8a80943e0c56198c
atlasfoo@lin <jf_mejiar547@live.com>
2021-05-27 00:16:47 -0600

    basic pages view

--- django_playground/settings.py
+++ django_playground/settings.py
@@ -45,7 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'ckeditor',
+    'core',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
